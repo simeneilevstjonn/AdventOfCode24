a3f3893793d563e8ca27011324478c105c6f02e9
Simen Eilevstjønn <simene@lyse.net>
2024-12-02 17:17:05 +0100

    Remove extra map

--- day2/day2_part2.rb
+++ day2/day2_part2.rb
@@ -1,1 +1,1 @@
-p $<.map{_1.split.map &:to_i}.count{|ir|(0...ir.size).map{|i|row=ir[...i]+ir[i+1..];n,x=row.each_cons(2).map{(_1-_2).abs}.minmax;(row==row.sort||row==row.sort.reverse)&&n>0&&x<4}.any?}
+p $<.count{|r|r=r.split.map(&:to_i);(0...r.size).map{|i|row=r[...i]+r[i+1..];n,x=row.each_cons(2).map{(_1-_2).abs}.minmax;(row==row.sort||row==row.sort.reverse)&&n>0&&x<4}.any?}
