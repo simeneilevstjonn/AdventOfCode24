638db4e10d42b0e28c77e293e6622b0fb24e7734
Simen Eilevstjønn <simene@lyse.net>
2024-12-02 17:18:22 +0100

    Remove long var names

--- day2/day2_part2.rb
+++ day2/day2_part2.rb
@@ -1,1 +1,1 @@
-p $<.count{|r|r=r.split.map(&:to_i);(0...r.size).map{|i|row=r[...i]+r[i+1..];n,x=row.each_cons(2).map{(_1-_2).abs}.minmax;(row==row.sort||row==row.sort.reverse)&&n>0&&x<4}.any?}
+p $<.count{|r|r=r.split.map(&:to_i);(0...r.size).map{|i|a=r[...i]+r[i+1..];n,x=a.each_cons(2).map{(_1-_2).abs}.minmax;(a==a.sort||a==a.sort.reverse)&&n>0&&x<4}.any?}
