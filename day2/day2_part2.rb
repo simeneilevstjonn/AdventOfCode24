de05784048cb62c784e14105552c3f6ee78f7274
Simen Eilevstjønn <simene@lyse.net>
2024-12-02 22:10:07 +0100

    Remove parentheses

--- day2/day2_part2.rb
+++ day2/day2_part2.rb
@@ -1,1 +1,1 @@
-p $<.count{|r|r=r.split.map(&:to_i);(0...r.size).map{|i|a=r[...i]+r[i+1..];n,x=(b=a.sort).each_cons(2).map{_2-_1}.minmax;(a==b||a==b.reverse)&&n>0&&x<4}.any?}
+p $<.count{|r|r=r.split.map &:to_i;(0...r.size).map{|i|a=r[...i]+r[i+1..];n,x=(b=a.sort).each_cons(2).map{_2-_1}.minmax;(a==b||a==b.reverse)&&n>0&&x<4}.any?}
