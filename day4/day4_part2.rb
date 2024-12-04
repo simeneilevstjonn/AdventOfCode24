7629697af59a1a6a67fc745899b82ced82c8cb31
Simen Eilevstjønn <simene@lyse.net>
2024-12-04 21:49:29 +0100

    Remove newlines

--- day4/day4_part2.rb
+++ day4/day4_part2.rb
@@ -1,3 +1,5 @@
-t=`dd`
-l=t.split("\n")[0].size-1
-p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{t.scan(/(?=#{_1}(?:.|\n){#{l}}A(?:.|\n){#{l}}#{_2})/).count}
+t= `dd`.split"
+"
+l=t.size-2
+t=t*""
+p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{t.scan(/(?=#{_1}.{#{l}}A.{#{l}}#{_2})/).count}
