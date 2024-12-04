9f94f6c5271319a72dbe4fc45067203aec5978b9
Simen Eilevstjønn <simene@lyse.net>
2024-12-04 21:34:45 +0100

    Day 4 part 1

--- day4/day4_part1.rb
+++ day4/day4_part1.rb
@@ -4,14 +4,14 @@ text = `dd`
 lineLen = text.split("\n")[0].size
 
 
-normal = text.count("XMAS")
-rev = text.count("SAMX")
-dn = text.scan(/X(?:.|\n){#{lineLen}}M(?:.|\n){#{lineLen}}A(?:.|\n){#{lineLen}}S/).size
-up = text.scan(/S(?:.|\n){#{lineLen}}A(?:.|\n){#{lineLen}}M(?:.|\n){#{lineLen}}X/).size
+p normal = text.scan(/XMAS/).size
+p rev = text.scan(/SAMX/).size
+p dn = text.scan(/(?=X(?:.|\n){#{lineLen}}M(?:.|\n){#{lineLen}}A(?:.|\n){#{lineLen}}S)/).size
+p up = text.scan(/(?=S(?:.|\n){#{lineLen}}A(?:.|\n){#{lineLen}}M(?:.|\n){#{lineLen}}X)/).size
 
 di = [-1, 1].sum{|i|
-  text.scan(/X(?:.|\n){#{lineLen + i}}M(?:.|\n){#{lineLen + i}}A(?:.|\n){#{lineLen + i}}S/).size +
-  text.scan(/S(?:.|\n){#{lineLen + i}}A(?:.|\n){#{lineLen + i}}M(?:.|\n){#{lineLen + i}}X/).size
+  (p text.scan(/(?=X(?:.|\n){#{lineLen + i}}M(?:.|\n){#{lineLen + i}}A(?:.|\n){#{lineLen + i}}S)/).size) +
+  (p text.scan(/(?=S(?:.|\n){#{lineLen + i}}A(?:.|\n){#{lineLen + i}}M(?:.|\n){#{lineLen + i}}X)/).size)
 }
 
 p normal + rev + dn + up + di
