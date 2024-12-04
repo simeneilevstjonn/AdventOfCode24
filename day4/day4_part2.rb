c8693e0796eab7d2829d5d64bce11f0014cd256e
Simen Eilevstjønn <simene@lyse.net>
2024-12-04 21:56:14 +0100

    Remove variables

--- day4/day4_part2.rb
+++ day4/day4_part2.rb
@@ -1,5 +1,3 @@
 t= `dd`.split"
 "
-l=t.size-2
-t=t*""
-p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{t.scan(/(?=#{_1}.{#{l}}A.{#{l}}#{_2})/).count}
+p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{t.join.scan(/(?=#{_1+[".{#{t.size-2}}"]*2*?A+_2})/).count}
