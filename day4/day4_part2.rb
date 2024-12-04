fe1472ec6bbaf30711cfe762eb2ea5f39224d63b
Simen Eilevstjønn <simene@lyse.net>
2024-12-04 21:44:11 +0100

    Golf day 4

--- day4/day4_part2.rb
+++ day4/day4_part2.rb
@@ -1,9 +1,3 @@
-text = `dd`
-
-
-lineLen = text.split("\n")[0].size
-
-
-p [["M.M","S.S"],["S.S","M.M"],["M.S","M.S"],["S.M","S.M"]].sum{|a,b|
-  text.scan(/(?=#{a}(?:.|\n){#{lineLen - 1}}A(?:.|\n){#{lineLen - 1}}#{b})/).count
-}
+t= `dd`
+l=t.split("\n")[0].size-1
+p [["M.M","S.S"],["S.S","M.M"],["M.S","M.S"],["S.M","S.M"]].sum{t.scan(/(?=#{_1}(?:.|\n){#{l}}A(?:.|\n){#{l}}#{_2})/).count}
