0326f2951cd8f5a360af83d15e90b198985a5b05
Simen Eilevstjønn <simene@lyse.net>
2024-12-04 22:12:31 +0100

    Fix off-by-2

--- day4/day4_part2.rb
+++ day4/day4_part2.rb
@@ -1,3 +1,3 @@
 t= `dd`.split"
 "
-p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{t.join.scan(/(?=#{_1+[".{#{t.size-2}}"]*2*?A+_2})/).count}
+p [%w(M.M S.S),%w(S.S M.M),%w(M.S M.S),%w(S.M S.M)].sum{(t*"aa").scan(/(?=#{_1+[".{#{t.size}}"]*2*?A+_2})/).count}
