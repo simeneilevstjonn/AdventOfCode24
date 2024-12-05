191b130d1bc72fcc35897ef70548d53b65e15b81
Simen Eilevstjønn <simene@lyse.net>
2024-12-05 17:44:55 +0100

    Golf loop

--- day5/day5_part2.rb
+++ day5/day5_part2.rb
@@ -3,8 +3,4 @@ r,d=`dd`.split("
 ").map{_1.split("
 ").map{|i|i.split(/\||,/).map &:to_i}}
 v=->(l,i){r.map{l[i]==_2&&l[i+1..].include?(_1)}.any?}
-I=d.filter{|l|(0...l.size).any?{v.(l,_1)}}
-p I.sum{|l|
-  (0...l.size).map{|i|(0..l.size-2).map{|j|v.(l,j)?(l[j],l[j+1]=l[j+1],l[j]):0}}
-  l[l.size/2]
-}
+p d.sum{|l|(0...l.size).any?{v.(l,_1)}?(l.map{(0..l.size-2).map{|j|v.(l,j)?(l[j],l[j+1]=l[j+1],l[j]):0}};l[l.size/2]):0}
