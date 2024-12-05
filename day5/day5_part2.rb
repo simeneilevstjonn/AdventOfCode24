bc6ff8825cfd6597e3e7c4dc6d1d0eb9cd24c7d1
Simen Eilevstjønn <simene@lyse.net>
2024-12-05 17:43:23 +0100

    shorten loop

--- day5/day5_part2.rb
+++ day5/day5_part2.rb
@@ -2,30 +2,9 @@ r,d=`dd`.split("
 
 ").map{_1.split("
 ").map{|i|i.split(/\||,/).map &:to_i}}
-
-rules = r
-data = d
-
 v=->(l,i){r.map{l[i]==_2&&l[i+1..].include?(_1)}.any?}
-
 I=d.filter{|l|(0...l.size).any?{v.(l,_1)}}
-
-p I.sum{|line|
-  ac = false
-  while !ac
-    ac = true
-    i = 0
-    while i < line.size - 1 do
-      if v.(line,i)
-        temp = line[i + 1]
-        line[i + 1] = line[i]
-        line[i] = temp
-        ac = false
-      end
-
-      i += 1
-    end
-  end
-
-  line[line.size / 2]
+p I.sum{|l|
+  (0...l.size).map{|i|(0..l.size-2).map{|j|v.(l,j)?(l[j],l[j+1]=l[j+1],l[j]):0}}
+  l[l.size/2]
 }
