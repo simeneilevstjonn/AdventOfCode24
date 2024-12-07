292d657d2acb5ba7a3a1a10c09e41bced25f8922
Simen Eilevstjønn <simene@lyse.net>
2024-12-07 11:33:04 +0100

    remove parantheses

--- day7/day7_part2.rb
+++ day7/day7_part2.rb
@@ -1,1 +1,1 @@
-p $<.sum{s,*n=_1.scan(/\d+/).map(&:to_i);(0...(3**(n.size-1))).any?{|i|j=-1;n.inject{|a,b|[a*b,a+b,"#{a}#{b}".to_i][(i/(3**(j+=1)))%3]}==s}?s:0}
+p $<.sum{s,*n=_1.scan(/\d+/).map &:to_i;(0...3**(n.size-1)).any?{|i|j=-1;n.inject{|a,b|[a*b,a+b,"#{a}#{b}".to_i][i/3**(j+=1)%3]}==s}?s:0}
