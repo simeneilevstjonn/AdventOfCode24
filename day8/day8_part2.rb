2671808f458c0820dfe927df675261db7961caf5
Simen Eilevstjønn <simene@lyse.net>
2024-12-08 11:14:16 +0100

    pre-varname golf

--- day8/day8_part2.rb
+++ day8/day8_part2.rb
@@ -1,22 +1,6 @@
-grid = $<.map{_1.chomp.chars}
-tx = grid.flat_map.with_index{|r,i|r.filter_map.with_index{|c,j|[i,j,c]if c!=?.}}
-an = []
-tx.combination(2).map{
-    i,j,c=_1;
-    y,x,d=_2;
-    if c==d
-        dy=y-i;
-        dx=x-j;
-        (0..).map{|m|
-            ny=i+m*dy;
-            nx=j+m*dx;
-            nY=i-m*dy;
-            nX=j-m*dx;
-            e=0
-            ny>=grid.size||nx>=grid[0].size||nx<0||ny<0 ?(e+=1):an.push([ny,nx])
-            nY>=grid.size||nX>=grid[0].size||nX<0||nY<0 ?(e+=1):an.push([nY,nX])
-            break if e>1
-        }
-    end
-}
+grid=$<.map{_1.chomp.chars}
+tx=grid.flat_map.with_index{|r,i|r.filter_map.with_index{|c,j|[i,j,c]if c!=?.}}
+an=[]
+A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=grid.size||f>=grid[0].size||f<0||e<0?1:(an.push([e,f]);0)}
+tx.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
 p an.uniq.size
