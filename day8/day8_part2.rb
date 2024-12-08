98313a97e16885e9471c1cdc030b8bce22cb09ee
Simen Eilevstjønn <simene@lyse.net>
2024-12-08 11:20:29 +0100

    inline input

--- day8/day8_part2.rb
+++ day8/day8_part2.rb
@@ -1,5 +1,5 @@
-g=$<.map{_1.chomp.chars}
+s=0
 n=[]
-A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=g.size||f>=g[0].size||f<0||e<0?1:(n.push([e,f]);0)}
-g.flat_map.with_index{|r,i|r.filter_map.with_index{[i,_2,_1]if _1!=?.}}.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
+A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=s||f>=s||f<0||e<0?1:(n.push([e,f]);0)}
+$<.flat_map{|r|s+=1;r.chomp.chars.filter_map.with_index{[s-1,_2,_1]if _1!=?.}}.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
 p n.uniq.size
