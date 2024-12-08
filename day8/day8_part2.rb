a4999083dab59cac5fc8e591afdb5174adb2faea
Simen Eilevstjønn <simene@lyse.net>
2024-12-08 11:24:00 +0100

    Init counter to -1

--- day8/day8_part2.rb
+++ day8/day8_part2.rb
@@ -1,5 +1,5 @@
-s=0
+s=-1
 n=[]
-A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=s||f>=s||f<0||e<0?1:(n.push([e,f]);0)}
-$<.flat_map{|r|s+=1;g=-1;r.chomp.chars.filter_map{g+=1;[s-1,g,_1]if _1!=?.}}.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
+A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>s||f>s||f<0||e<0?1:(n.push [e,f];0)}
+$<.flat_map{|r|s+=1;g=-1;r.chomp.chars.filter_map{g+=1;[s,g,_1]if _1!=?.}}.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
 p n.uniq.size
