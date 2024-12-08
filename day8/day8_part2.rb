7ebe40dab1742d095852385fa9d9a50cdb71f46f
Simen Eilevstjønn <simene@lyse.net>
2024-12-08 11:15:54 +0100

    Golf some var names

--- day8/day8_part2.rb
+++ day8/day8_part2.rb
@@ -1,6 +1,5 @@
-grid=$<.map{_1.chomp.chars}
-tx=grid.flat_map.with_index{|r,i|r.filter_map.with_index{|c,j|[i,j,c]if c!=?.}}
-an=[]
-A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=grid.size||f>=grid[0].size||f<0||e<0?1:(an.push([e,f]);0)}
-tx.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
-p an.uniq.size
+g=$<.map{_1.chomp.chars}
+n=[]
+A=->(i,j,c,y,x,d,m){e=i+m*(y-i);f=j+m*(x-j);c!=d||e>=g.size||f>=g[0].size||f<0||e<0?1:(n.push([e,f]);0)}
+g.flat_map.with_index{|r,i|r.filter_map.with_index{|c,j|[i,j,c]if c!=?.}}.combination(2).map{|a,b|(0..).map{break if A.(*a,*b,_1)+A.(*a,*b,-_1)>1}}
+p n.uniq.size
